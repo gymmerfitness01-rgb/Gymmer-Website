32ffbc1b2b01752fe4f3c4044d04dfef46f45d8a
Indian-Coder <indiancoder92@gmail.com>
2025-11-20 21:30:27 +0530

    Updated Not Found Text

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-white px-4">
             <h1 className="text-4xl font-bold text-gray-900 mb-4">404 — Wrong Rep!</h1>
-            <p className="text-gray-600 mb-6">You took a wrong turn. Let’s get you back on track for your progress.</p>
+            <p className="text-gray-600 mb-6 text-center">Off track? Let’s get you back to progress.</p>
 
             <a
                 href="/"
